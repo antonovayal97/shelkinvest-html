3b481c8ae28ae51da2de44ffaf330e1bbb25610b
Aial <antonovayal97@gmail.com>
2025-11-24 15:49:49 +0900

    init

---
+++ js/common.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  let mainBanner = document.querySelector(".main-banner");
+  let animationDuration = 750;
+  let animationDelayLogo = 150;
+  let animationType = "ease-in-out";
+
+  if (mainBanner) {
+    let mainBannerImage = mainBanner.querySelector(".main-banner__image img");
+    let mainBannerBottomImage = mainBanner.querySelector(
+      ".main-banner__bottom-image"
+    );
+    let mainBannerLogo = mainBanner.querySelector(".main-banner__top-logo");
+
+    let mainBannerBottomShadowOne = mainBanner.querySelector(
+      ".main-banner__bottom-shadow-1"
+    );
+    let mainBannerBottomShadowTwo = mainBanner.querySelector(
+      ".main-banner__bottom-shadow-2"
+    );
+
+    if (
+      mainBannerImage &&
+      mainBannerBottomImage &&
+      mainBannerLogo &&
+      mainBannerBottomShadowOne &&
+      mainBannerBottomShadowTwo
+    ) {
+      mainBannerImage.style.transition =
+        "all " + animationDuration + "ms " + animationType;
+      mainBannerBottomImage.style.transition =
+        "all " + animationDuration + "ms " + animationType;
+
+      mainBannerLogo.style.transition =
+        "all " + animationDuration + "ms " + animationType;
+      mainBannerLogo.style.transitionDelay = animationDelayLogo;
+
+      mainBannerBottomShadowOne.style.transition =
+        "all " + animationDuration + "ms " + animationType;
+      mainBannerBottomShadowOne.style.transitionDelay = animationDelayLogo;
+
+      mainBannerBottomShadowTwo.style.transition =
+        "all " + animationDuration + "ms " + animationType;
+      mainBannerBottomShadowTwo.style.transitionDelay = animationDelayLogo;
+
+      mainBanner.classList.add("main-banner--animated");
+    }
+  }
+});
