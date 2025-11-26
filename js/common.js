3cbe064ce58dd14484df08356567c35ace71fb3d
Aial <antonovayal97@gmail.com>
2025-11-26 13:45:41 +0900

    fix

--- js/common.js
+++ js/common.js
@@ -1,1 +1,19 @@
-document.addEventListener("DOMContentLoaded", (event) => {});
+function hidePageLoader() {
+  const pageLoader = document.querySelector(".page-loader");
+  if (pageLoader) {
+    pageLoader.classList.add("loaded");
+    pageLoader.addEventListener(
+      "transitionend",
+      () => {
+        if (pageLoader.classList.contains("loaded")) {
+          pageLoader.remove();
+        }
+      },
+      { once: true }
+    );
+  }
+}
+
+window.addEventListener("load", () => {
+  setTimeout(hidePageLoader, 100);
+});
