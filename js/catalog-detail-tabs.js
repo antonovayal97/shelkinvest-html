050b4a24fabff31c9f3b1cc68321c913fab45514
Aial <antonovayal97@gmail.com>
2025-12-08 15:34:54 +0900

    fix

---
+++ js/catalog-detail-tabs.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let tabs = document.querySelectorAll(".p-catalog-detail-2__tab");
+  let contents = document.querySelectorAll(".p-catalog-detail-2__tabs-content");
+  let current_content = contents[0];
+  let current_tab = tabs[0];
+
+  current_tab.classList.add("p-catalog-detail-2__tab--active");
+  current_content.classList.add("p-catalog-detail-2__tabs-content--active");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      if (!tab.classList.contains("p-catalog-detail-2__tab--active")) {
+        current_tab.classList.remove("p-catalog-detail-2__tab--active");
+        current_tab = tabs[index];
+        tab.classList.add("p-catalog-detail-2__tab--active");
+
+        current_content.classList.remove(
+          "p-catalog-detail-2__tabs-content--active"
+        );
+        current_content = contents[index];
+        current_content.classList.add(
+          "p-catalog-detail-2__tabs-content--active"
+        );
+      }
+    });
+  });
+});
